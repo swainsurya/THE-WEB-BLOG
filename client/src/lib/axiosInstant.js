883b2a0c14ce0e2c96afead244128fbb77264357
surya <swainsuryakanta97@gmail.com>
2025-02-18 00:10:30 +0530

    redirect error

--- client/src/lib/axiosInstant.js
+++ client/src/lib/axiosInstant.js
@@ -3,6 +3,7 @@ import axios from "axios";
 export const axiosIntance =  axios.create({
     baseURL:import.meta.env.VITE_SERVER,
     withCredentials : true,
+    maxRedirects : 0,
     headers : {
         "Content-Type" : "application/json"
     },
