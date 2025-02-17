10e347aa481d9ff741f66f5d13c0929cb7bd6c8c
surya <swainsuryakanta97@gmail.com>
2025-02-17 20:35:37 +0530

    indexjs updated

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(clerkMiddleware({
-    authorizedParties: "https://the-web-blog.onrender.com"
+    authorizedParties: ["https://the-web-blog.onrender.com"]
 }));
 
 // app.use("/api/user", userRoute)
